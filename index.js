7b0c49192ffbaf09d1f4aa325a09620ec0231a3d
breaker05 <chriskeenan@gmail.com>
2023-08-09 22:31:23 -0400

    auth

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-
-const serveIndex = require('serve-index');
-
 const express = require('express');
+const serveIndex = require('serve-index');
+const auth = require('http-auth');
+const authConnect = require("http-auth-connect");
+
+var basic = auth.basic({
+  realm: 'SUPER SECRET STUFF'
+}, function (username, password, callback) {
+  callback(username == '' && password == '')
+});
+
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.use(authConnect(basic));
 
 app.use('/', express.static('D:\\tools'), serveIndex('D:\\tools', {'icons': true}));
 
